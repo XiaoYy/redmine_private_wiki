5547a7be67f6ab3621d77a411b6f2ee34a55a036
mengfanbin <dreamfanbin@gmail.com>
2014-07-04 10:36:46 +0800

    fix bug for search issue

--- lib/private_wiki/search_controller_patch.rb
+++ lib/private_wiki/search_controller_patch.rb
@@ -10,7 +10,7 @@ module PrivateWiki
     module InstanceMethods
       def index_with_private_wiki
         index_without_private_wiki
-        @results.delete_if do |result|
+        @results && @results.delete_if do |result|
           if result.respond_to?(:private) && result.respond_to?(:private_page_visible?)
             result.private and !result.private_page_visible? @project, User.current
           end
